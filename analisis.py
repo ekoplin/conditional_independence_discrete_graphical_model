2e340be3a72501ba9ddf67d53d201f88aef18e48
ekoplin <ericlkoplin@gmail.com>
2024-08-08 13:39:21 -0500

    minimal

--- analisis.py
+++ analisis.py
@@ -5,21 +5,21 @@ Created on Wed Jul 31 13:50:40 2024
 
 @author: eric
 """
-# import pandas as pd
+import pandas as pd
 import os
 import numpy as np 
 from discrete_gm_nonpos import discrete_graphical_model
 
-# WS = pd.read_excel('ecu_ecv13-14.xlsx')
-# X = WS[['d_cm','d_nutr','d_satt','d_educ','d_elct','d_wtr','d_sani','d_hsg','d_ckfl','d_asst']].to_numpy()
+WS = pd.read_excel('ecu_ecv13-14.xlsx')
+X = WS[['d_cm','d_nutr','d_satt','d_educ','d_elct','d_wtr','d_sani','d_hsg','d_ckfl','d_asst']].to_numpy()
 
-# Ynone = np.zeros((X.shape[0],0))
-# Yarea = pd.Categorical(WS.area).codes.reshape(X.shape[0],1)
-# Yregion = pd.get_dummies(pd.Categorical(WS.region)).to_numpy()# convert to binary multivariate
+Ynone = np.zeros((X.shape[0],0))
+Yarea = pd.Categorical(WS.area).codes.reshape(X.shape[0],1)
+Yregion = pd.get_dummies(pd.Categorical(WS.region)).to_numpy()# convert to binary multivariate
 
-# data = {'X':X,'Ynone':Ynone,'Yarea':Yarea,'Yregion':Yregion}
+data = {'X':X,'Ynone':Ynone,'Yarea':Yarea,'Yregion':Yregion}
 # np.savez('ecu_ecv13-14.npz',**data)
-data=np.load('ecu_ecv13-14.npz')
+# data=np.load('ecu_ecv13-14.npz')
 
 
 #for (Y,name) in [(Ynone,"none"), (Yarea,"area"),(Yregion,"region")]:
@@ -30,8 +30,8 @@ for name in ["Ynone", "Yarea","Yregion"]:
     Xclean = X[~indx_nan,:]
     Yclean = Y[~indx_nan,:]
     ci=discrete_graphical_model(np.linspace(1, 10,10)).estimate_CI(Xclean>0, Yclean>0)# only binary data allowed
-    os.mkdir("./ecu_ecv13-14_covar/"+name)
+    os.mkdir("./ecu_ecv13-14_covar"+name)
     for ic in range(len(ci['conserv'])):
         np.savetxt("./ecu_ecv13-14_covar"+name+"/conservative_c"+str(ic)+".txt", ci['conserv'][ic] , fmt="%5i")
-        np.savetxt("./ecu_ecv13-14_covar/"+name+"/nconservative_c"+str(ic)+".txt", ci['nconserv'][ic] , fmt="%5i")
+        np.savetxt("./ecu_ecv13-14_covar"+name+"/nconservative_c"+str(ic)+".txt", ci['nconserv'][ic] , fmt="%5i")
 
