37c9b37f0435a62c420d5c64128fac36c6792420
ekoplin <ericlkoplin@gmail.com>
2024-08-08 13:29:41 -0500

    parallel implementation with vector of c parameter

--- analisis.py
+++ analisis.py
@@ -5,23 +5,33 @@ Created on Wed Jul 31 13:50:40 2024
 
 @author: eric
 """
-import pandas as pd
+# import pandas as pd
+import os
 import numpy as np 
 from discrete_gm_nonpos import discrete_graphical_model
 
-WS = pd.read_excel('ecu_ecv13-14.xlsx')
+# WS = pd.read_excel('ecu_ecv13-14.xlsx')
+# X = WS[['d_cm','d_nutr','d_satt','d_educ','d_elct','d_wtr','d_sani','d_hsg','d_ckfl','d_asst']].to_numpy()
+
+# Ynone = np.zeros((X.shape[0],0))
+# Yarea = pd.Categorical(WS.area).codes.reshape(X.shape[0],1)
+# Yregion = pd.get_dummies(pd.Categorical(WS.region)).to_numpy()# convert to binary multivariate
+
+# data = {'X':X,'Ynone':Ynone,'Yarea':Yarea,'Yregion':Yregion}
+# np.savez('ecu_ecv13-14.npz',**data)
+data=np.load('ecu_ecv13-14.npz')
 
 
-X = WS[['d_cm','d_nutr','d_satt','d_educ','d_elct','d_wtr','d_sani','d_hsg','d_ckfl','d_asst']].to_numpy()
-
-Ynone = np.zeros((X.shape[0],0))
-Yarea = pd.Categorical(WS.area).codes.reshape(X.shape[0],1)
-Yregion = pd.get_dummies(pd.Categorical(WS.region)).to_numpy()# convert to binary multivariate
-
-for (Y,name) in [(Ynone,"none"), (Yarea,"area"),(Yregion,"region")]:
+#for (Y,name) in [(Ynone,"none"), (Yarea,"area"),(Yregion,"region")]:
+for name in ["Ynone", "Yarea","Yregion"]:
+    Y=data[name]
+    X=data['X']
     indx_nan=np.isnan(X).any(1)|np.isnan(Y).any(1)
     Xclean = X[~indx_nan,:]
     Yclean = Y[~indx_nan,:]
-    ci=discrete_graphical_model(c=.1).estimate_CI(Xclean>0, Yclean>0)# only binary data allowed
-    np.savetxt("./ecu_ecv13-14_covar-"+name+".txt", ci , fmt="%5i")
+    ci=discrete_graphical_model(np.linspace(1, 10,10)).estimate_CI(Xclean>0, Yclean>0)# only binary data allowed
+    os.mkdir("./ecu_ecv13-14_covar/")
+    for ic in range(len(ci['conserv'])):
+        np.savetxt("./ecu_ecv13-14_covar/"+name+"_conservative_c"+str(ic)+".txt", ci['conserv'][ic] , fmt="%5i")
+        np.savetxt("./ecu_ecv13-14_covar/"+name+"_nconservative_c"+str(ic)+".txt", ci['nconserv'][ic] , fmt="%5i")
 
--- discrete_gm_nonpos.py
+++ discrete_gm_nonpos.py
@@ -8,14 +8,16 @@ Created on Tue Jul 30 19:33:07 2024
 
 import numpy as np
 from itertools import combinations
+import multiprocessing
+from functools import partial
 def int2bin(x, bits):
     return np.array([int(i) for i in bin(x)[2:].zfill(bits)])
 
 class discrete_graphical_model:
-    def __init__(self,c=.1,conservative=False):
-        self.c = c
-        self.conservative = conservative
-    def _bic(self,X,indx_v,indx_w):
+    def __init__(self,c=np.linspace(.1,1,10),ncores=None):
+        self.c = c.reshape(-1,1)# column
+        self.ncores = ncores
+    def _bic(self,X,indx_v,indx_w,ne_size):
         Xnz = np.zeros_like(X)
         Xnz[:,indx_v+indx_w]=X[:,indx_v+indx_w]
         n,p = X.shape
@@ -41,36 +43,53 @@ class discrete_graphical_model:
         for key in keys:
             lpl += logP_av_given_aw[key]*N_av_aw[key]
         
-        bic = lpl - self.c*pow(len(np.unique(X)),len(indx_w))*np.log(n)
+        #bic = lpl - self.c*pow(len(np.unique(X)),len(indx_w))*np.log(n)
+        bic = lpl - self.c*pow(len(np.unique(X)),ne_size)*np.log(n)
         return(bic)
+    def compute_ne_i(self,i,X,Y):
+        YX = np.hstack((Y,X))
+        q = Y.shape[1]# covariates
+        p = X.shape[1]
+        
+        indx_v = [i]
+        indx_all = set(range(p)) 
+        indx_no_v =indx_all.difference(indx_v)
+        
+        bic_v = []
+        ne_v  = []
+        for ne in range(len(indx_all)):
+            for indx_w in combinations(indx_no_v, ne):
+                ne_v.append(indx_w)
+                bic_v.append(self._bic(YX,[q+i for i in indx_v],list(range(q))+[q+j for j in indx_w],len(indx_w)))
+        #ne_v_optim = ne_v[max(enumerate(bic_v), key=lambda x: x[1])[0]]
+        #NE[i,ne_v_optim]=True
+        ne_v_optim_indx=np.argmax(np.hstack(bic_v),axis=1,keepdims=True)
+        ne_v_optim     = np.zeros((len(self.c),p),dtype=bool)
+        for ic in range(len(self.c)):
+            ne_v_optim[ic,ne_v[int(ne_v_optim_indx[ic])]]=True 
+        #NElst.append(ne_v_optim)
+        return(ne_v_optim)
     def estimate_CI(self,X,Y):
         # estimate the neighbourhood of every index
         # X predictors
         # Y covariates
         # c positive constant (regularization)
-        YX = np.hstack((Y,X))
-        q = Y.shape[1]# covariates
-        p = X.shape[1]
         
-        NE = np.zeros((p,p),dtype=bool)
-        indx_all = set(range(p)) 
-        for i in range(p):
-            indx_v = [i]
-            indx_no_v =indx_all.difference(indx_v)
-            
-            bic_v = []
-            ne_v  = []
-            for ne in range(len(indx_all)):
-                for indx_w in combinations(indx_no_v, ne):
-                    ne_v.append(indx_w)
-                    bic_v.append(self._bic(YX,[q+i for i in indx_v],list(range(q))+[q+j for j in indx_w]))
-            ne_v_optim = ne_v[max(enumerate(bic_v), key=lambda x: x[1])[0]]
-            
-            NE[i,ne_v_optim]=True
         
+        #NElst = list()
+        
+        
+        with multiprocessing.Pool(self.ncores) as pool:
+            NElst=pool.map(partial(self.compute_ne_i,X=X,Y=Y), range(X.shape[1]))
+        
+            
+        NE=np.stack(NElst,2)# |c|xpxp <=> c,neighbor, variable
         # simetrization
-        if self.conservative:
-            NE = NE & np.transpose(NE)
-        else:
-            NE = NE | np.transpose(NE)
-        return(NE)
+        NE_conserv  = NE & np.moveaxis(NE,-1,-2)
+        NE_nconserv = NE | np.moveaxis(NE,-1,-2)
+        return({'conserv' : np.split(NE_conserv, 1, 0)[0], 'nconserv' : np.split(NE_nconserv, 1, 0)[0]})
+        # if self.conservative:
+        #     NE = NE & np.transpose(NE)
+        # else:
+        #     NE = NE | np.transpose(NE)
+        # return(NE)
